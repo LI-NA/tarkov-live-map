dc952b7d86b0abe39a7f2edb9e712e892c63aa53
LI-NA <hyung778@pm.me>
2024-04-21 21:25:18 +0900

    feat(font): Add pretendard font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config = {
             },
         },
         extend: {
+            fontFamily: {
+                sans: ["var(--font-pretendard)"],
+            },
             colors: {
                 border: "hsl(var(--border))",
                 input: "hsl(var(--input))",
