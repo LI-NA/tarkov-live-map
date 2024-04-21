cebad80f69a5c191d1318fe963d78565903ec89d
LI-NA <hyung778@pm.me>
2024-04-21 18:53:42 +0900

    docs: Update readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tarkov-live-map
+
 A project that allows the user to view a map in the game Escape From Tarkov and show the current location and share it with friends.
+
+This project is currently in the startup phase. Nothing is working and is under development. If you have any questions, please open an issue.
--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
                     >
                         By{" "}
                         <Image
-                            src="/vercel.svg"
+                            src="/tarkov-live-map/vercel.svg"
                             alt="Vercel Logo"
                             className="dark:invert"
                             width={100}
